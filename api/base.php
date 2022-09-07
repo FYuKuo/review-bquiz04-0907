032e7569d2a38fa8e9ad3739d835b2d40523a6c2
FYKuo <teresa99339@gmail.com>
2022-09-07 14:09:51 +0800

    整理檔案 完成base檔

---
+++ api/base.php
@@ -0,0 +1,147 @@
+<?php
+date_default_timezone_set('Asia/Taipei');
+session_start();
+
+class DB
+{
+
+    protected $table;
+    protected $pdo;
+    protected $dsn = "mysql:host=localhost;chartset=utf8;dbname=db15-0907";
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn,'root','');
+    }
+
+    public function all(...$arg)
+    {
+        $sql = "SELECT * FROM `$this->table` ";
+
+        if(isset($arg[0])){
+            
+            if(is_array($arg[0])){
+
+                foreach ($arg[0] as $key => $value) {
+                    $tmp[] = " `$key` = '$value' ";
+                }
+
+                $sql .= " WHERE " . join('AND',$tmp);
+            }else{
+
+            $sql .= $arg[0];
+
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .= $arg[1];
+        }
+
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function find($id)
+    {
+        $sql = "SELECT * FROM `$this->table` ";
+
+            if(is_array($id)){
+
+                foreach ($id as $key => $value) {
+                    $tmp[] = " `$key` = '$value' ";
+                }
+
+                $sql .= " WHERE " . join('AND',$tmp);
+            }else{
+
+            $sql .= " WHERE `id` = '$id' ";
+
+            }
+        
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+    public function del($id)
+    {
+        $sql = "DELETE FROM `$this->table` ";
+
+            if(is_array($id)){
+
+                foreach ($id as $key => $value) {
+                    $tmp[] = " `$key` = '$value' ";
+                }
+
+                $sql .= " WHERE " . join('AND',$tmp);
+            }else{
+
+            $sql .= " WHERE `id` = '$id' ";
+
+            }
+        
+        return $this->pdo->exec($sql);
+    }
+
+
+    public function save($array)
+    {
+        if(isset($array['id'])){
+
+            foreach ($array as $key => $value) {
+                if($key != 'id'){
+                    $tmp[] = " `$key` = '$value' ";
+                }
+            }
+
+            $sql = "UPDATE `$this->table`" . join(',',$tmp) . " WHERE `id` = '{$array['id']}' ";
+        }else{
+            $col = join("`,`",array_keys($array));
+            $val = join("','",$array);
+
+            $sql = "INSERT INTO `$this->table` (`$col`) VALUES ('$val')";
+        }
+        return $this->pdo->exec($sql);
+    }
+
+    public function math($math,$col,...$arg)
+    {
+        $sql = "SELECT $math($col) FROM `$this->table` ";
+
+        if(isset($arg[0])){
+            
+            if(is_array($arg[0])){
+
+                foreach ($arg[0] as $key => $value) {
+                    $tmp[] = " `$key` = '$value' ";
+                }
+
+                $sql .= " WHERE " . join('AND',$tmp);
+            }else{
+
+            $sql .= $arg[0];
+
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .= $arg[1];
+        }
+
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+}
+
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function to($url)
+{
+    header("location:$url");
+}
+?>
